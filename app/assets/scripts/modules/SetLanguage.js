430de22571a66cf6e816851d1b15324d6331ddcc
Anibal Barca <anibal@centauro.com>
2020-05-22 09:57:56 -0500

    Added stairting files

---
+++ app/assets/scripts/modules/SetLanguage.js
@@ -0,0 +1,14 @@
+class SetLanguage {
+    constructor() {
+
+    }
+    let langData = {
+        "languages": {
+            "en": {
+                "strings": {
+                    "main-title": "coffee in the morning"
+                }
+            }
+        }
+    }
+}
