20bffbeb5d98fbe94760fb8a3c209ba36e21d0ae
edgarnava80 <edgarnr@hotmail.com>
2020-05-25 21:22:54 -0500

    Reveal on scroll added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,5 @@
 import '../styles/styles.css'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".site-main__white"), 70)
+new RevealOnScroll(document.querySelectorAll(".site-main__black"), 50)
