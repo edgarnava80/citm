ef0282ca21c45a2824d46b3c3b3b298ad2b2fdc4
edgarnava80 <edgarnr@hotmail.com>
2020-05-27 14:29:16 -0500

    Preloader added and menu fixed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,6 @@ import '../styles/styles.css'
 import Menu from './modules/Menu'
 import RevealOnScroll from './modules/RevealOnScroll'
 
-
-
 new RevealOnScroll(document.querySelectorAll(".site-main__white"), 70)
 new RevealOnScroll(document.querySelectorAll(".site-main__black"), 50)
 
