15c366ebb108f1595f78d3260e4da1c58019ae80
edgarnava80 <edgarnr@hotmail.com>
2020-05-27 21:07:38 -0500

    Foto gallery added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,8 @@
 import '../styles/styles.css'
-import Menu from './modules/Menu'
-import RevealOnScroll from './modules/RevealOnScroll'
+//import Menu from './modules/Menu'
+//import RevealOnScroll from './modules/RevealOnScroll'
 
-new RevealOnScroll(document.querySelectorAll(".site-main__white"), 70)
-new RevealOnScroll(document.querySelectorAll(".site-main__black"), 50)
+//new RevealOnScroll(document.querySelectorAll(".site-main__white"), 70)
+//new RevealOnScroll(document.querySelectorAll(".site-main__black"), 50)
 
-let appMenu = new Menu();
+//let appMenu = new Menu();
