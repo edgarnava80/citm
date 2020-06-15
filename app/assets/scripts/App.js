73186f5f1a72538e13dca42c8f536b562f258873
edgarnava80 <edgarnr@hotmail.com>
2020-06-14 20:38:17 -0500

    Fixed contact, home and scenes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -10,9 +10,10 @@ const scroll = new ScrollManager()
 new Menu()
 new Sound()
 new Cursor()
-imageGallery()
+imageGalleryH()
+imageGalleryV()
     //new Slider('.slide', '#arrow-left', '#arrow-right')
-function imageGallery() {
+function imageGalleryH() {
     const highlight = document.querySelector('.gallery-highlight')
     const previews = document.querySelectorAll('.image-preview img')
     previews.forEach(preview => {
@@ -22,6 +23,20 @@ function imageGallery() {
             highlight.src = bigSrc
             previews.forEach(el => el.classList.remove("image-active"))
             preview.classList.add("image-active")
+        })
+    })
+}
+
+function imageGalleryV() {
+    const highlight = document.querySelector('.vertical-highlight')
+    const previews = document.querySelectorAll('.vertical-preview img')
+    previews.forEach(preview => {
+        preview.addEventListener('click', function() {
+            const smallSrc = this.src
+            const bigSrc = smallSrc.replace('small', 'big')
+            highlight.src = bigSrc
+            previews.forEach(el => el.classList.remove("vertical-active"))
+            preview.classList.add("vertical-active")
         })
     })
 }
